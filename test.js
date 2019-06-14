753751ce5487aae0f003f9df1198193edde05b14
bukinoshita <bukinoshita@gmail.com>
2019-06-14 19:39:10 -0300

    test: fix tests

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-'use strict'
-
+// Packages
 import test from 'ava'
 import pokemonF from 'pokemon-f'
-import m from './'
 
-test(async t => {
+// Root
+import m from '.'
+
+test('pokebal shake', async t => {
   const catchRate = 163
   const hpMax = 35
   const hpCurrent = 29
@@ -18,6 +19,7 @@ test('burned', async t => {
   const hpMax = 35
   const hpCurrent = 29
   const f = await pokemonF(hpMax, 'pokeball', hpCurrent)
+
   t.is(await m(catchRate, 'pokeball', f, 12), 2)
 })
 
@@ -26,6 +28,7 @@ test('frozen', async t => {
   const hpMax = 35
   const hpCurrent = 29
   const f = await pokemonF(hpMax, 'pokeball', hpCurrent)
+
   t.is(await m(catchRate, 'pokeball', f, 25), 2)
 })
 
@@ -34,7 +37,7 @@ test('catchRate error', async t => {
   const hpMax = 35
   const hpCurrent = 29
   const f = await pokemonF(hpMax, 'pokeball', hpCurrent)
-  const error = await t.throws(m(catchRate, 'pokeball', f))
+  const error = await t.throwsAsync(m(catchRate, 'pokeball', f))
 
   t.is(error.message, 'Catch Rate is required and should be a number')
 })
@@ -44,14 +47,14 @@ test('pokeball error', async t => {
   const hpMax = 35
   const hpCurrent = 29
   const f = await pokemonF(hpMax, 'pokeball', hpCurrent)
-  const error = await t.throws(m(catchRate, 2, f))
+  const error = await t.throwsAsync(m(catchRate, 2, f))
 
   t.is(error.message, 'Pokeball is required and should be a string')
 })
 
 test('f error', async t => {
   const catchRate = 163
-  const error = await t.throws(m(catchRate, 'pokeball', 256))
+  const error = await t.throwsAsync(m(catchRate, 'pokeball', 256))
 
   t.is(error.message, 'f is required and should be a number between 1-255')
 })
